dfcbd3e0cafce290d226646346b219c51e3d555b
Josh Leath <lawnfurniture@icloud.com>
2020-08-28 20:34:58 -0400

    completed more exercises

--- small-problems/easy-4/running_totals.rb
+++ small-problems/easy-4/running_totals.rb
@@ -6,7 +6,24 @@ def running_total(arr)
   end
 end
 
-p running_total([2, 5, 13])
-p running_total([14,11,7,15,20])
-p running_total([3])
-p running_total([])
+def running_total_ewo(arr)
+  curr_total = 0
+  arr.each_with_object([]) do |n, result|
+    curr_total += n
+    result << curr_total
+  end
+end
+
+def running_total_inject(arr)
+  result = []
+  arr.inject(0) do |n, sum|
+    result << (sum + n)
+    sum + n
+  end
+  result
+end
+
+p running_total_inject([2, 5, 13])
+p running_total_inject([14,11,7,15,20])
+p running_total_inject([3])
+p running_total_inject([])
---
+++ small-problems/easy-4/s_to_i.rb
@@ -0,0 +1,34 @@
+# may not use #to_i or the Integer constructor
+# assumes all characters are valid
+DIGITS_MAP = {
+  '0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4, '5' => 5,
+  '6' => 6, '7' => 7, '8' => 8, '9' => 9, 'a' => 10, 'b' => 11,
+  'c' => 12, 'd' => 13, 'e' => 14, 'f' => 15
+}
+
+def string_to_i(text, base=10)
+  result = 0
+  digits = text.split('').map { |c| DIGITS_MAP[c.downcase] }
+  digits.each { |d| result = (result * base) + d }
+  result
+end
+
+# base 10
+def s_to_i(text)
+  string_to_i(text)
+end
+
+# base 16
+def h_to_i(text)
+  string_to_i(text, 16)
+end
+
+# base 8
+def o_to_i(text)
+  string_to_i(text, 8)
+end
+
+puts s_to_i('4321') == 4321
+puts s_to_i('570') == 570
+puts h_to_i('4D9f') == 19871
+puts o_to_i('427630') == 143256
