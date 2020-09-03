6aa9fe85c08e77dcf3a9e66ae7db2c2b3077c89f
Joshua Leath <lawnfurniture@icloud.com>
2020-09-02 20:41:24 -0400

    started easy-6

---
+++ small-problems/easy-6/delete_vowels.rb
@@ -0,0 +1,8 @@
+def remove_vowels(arr)
+  arr.map { |s| s.delete("aeiouAEIOU") }
+end
+
+
+puts remove_vowels(%w(abcdefghijklmnopqrstuvwxyz)) == %w(bcdfghjklmnpqrstvwxyz)
+puts remove_vowels(%w(green YELLOW black white)) == %w(grn YLLW blck wht)
+puts remove_vowels(%w(ABC AEIOU XYZ)) == ['BC', '', 'XYZ']
