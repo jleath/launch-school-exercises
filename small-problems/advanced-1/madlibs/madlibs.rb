b71caae08daa172a0f3ab5646e8714efd4df0fe0
Josh Leath <lawnfurniture@icloud.com>
2020-10-15 14:48:13 -0400

    finished madlibs exercise

--- small-problems/advanced-1/madlibs/madlibs.rb
+++ small-problems/advanced-1/madlibs/madlibs.rb
@@ -13,23 +13,28 @@ def read_file(filename)
 end
 
 def madlib_pattern?(word)
-  !!(/^#\{\w+\}$/.match(word))
+  !!(/%\{\w+\}/.match(word))
 end
 
 def get_madlib_input(word)
-  word_type = word.delete('#{}')
+  word_type = word.delete('%{}')
   print("Enter one #{word_type}: ")
   gets.chomp
 end
 
 def prompt_for_words(text)
+  output = []
   text.split.each do |word|
     if madlib_pattern?(word)
-      word_type = word.delete('#{}')
+      word_type = /%\{\w+\}/.match(word).to_s.delete('^a-zA-Z')
       print("Enter one #{word_type}: ")
-      madlib_words[word_type] << gets.chomp
+      output << word.sub(/%\{\w+\}/, gets.chomp)
+    else
+      output << word
     end
   end
+  output.join(' ')
 end
 
-puts /^#\{\w+\}$/.match('#noun}')
+madlib_text = read_file('madlib1.txt')
+puts prompt_for_words(madlib_text)
