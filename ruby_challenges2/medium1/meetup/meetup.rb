8772f11ba605b2968efe77fc898d11856729df01
Joshua Leath <lawnfurniture@icloud.com>
2020-12-19 08:20:15 -0500

    refactoring

--- ruby_challenges2/medium1/meetup/meetup.rb
+++ ruby_challenges2/medium1/meetup/meetup.rb
@@ -1,15 +1,17 @@
+require 'date'
+
 class Meetup
-  WEEKDAYS = {sunday: 0, monday: 1, tuesday: 2, wednesday: 3,
-              thursday: 4, friday: 5, saturday: 6}
+  WEEKDAYS = { sunday: 0,   monday: 1, tuesday: 2, wednesday: 3,
+               thursday: 4, friday: 5, saturday: 6 }.freeze
 
   SCHEDULE_FNS = {
-    first:  Proc.new { |days| days[0] },
-    second: Proc.new { |days| days[1] },
-    third:  Proc.new { |days| days[2] },
-    fourth: Proc.new { |days| days[3] },
-    last:   Proc.new { |days| days.last },
-    teenth: Proc.new { |days| days.detect { |date| (13..19).cover?(date.day) }}
-  } 
+    first:  proc { |days| days[0] },
+    second: proc { |days| days[1] },
+    third:  proc { |days| days[2] },
+    fourth: proc { |days| days[3] },
+    last:   proc { |days| days.last },
+    teenth: proc { |days| days.detect { |date| (13..19).cover?(date.day) } }
+  }.freeze
 
   def initialize(month, year)
     @month = month
@@ -21,7 +23,7 @@ class Meetup
   end
 
   def get_weekdays(weekday)
-    first_day = Date.new(@year, @month, 1)
+    first_day = Date.new(@year, @month)
     first_weekday = first_day + (WEEKDAYS[weekday] - first_day.wday) % 7
     month_end = first_day.next_month - 1
     first_weekday.step(month_end, 7).to_a
